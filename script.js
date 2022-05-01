bb1ac141b14f5bb4b98421a7045942abb1a45c65
Gabriel <gabrielcpa123@gmail.com>
2022-05-01 11:40:02 -0300

    movimento completo

--- script.js
+++ script.js
@@ -11,23 +11,26 @@ function start() {  // inicio função start
 // Variaveis
 
 var jogo = {};
+var velocidade = 5;
+var posicaoY = parseInt(Math.random()* 334);
 
 var TECLA = {
     W: 87,
     S: 83,
-    D: 68
+    D: 68,
 }
 
+
+// Verifica se o usuário pressionou alguma tecla
+
 jogo.pressionou = [];
-// Verifica se o usuário pressionou alguma tecla	
 
-
-
-$(document).keydown(function(e) {
+$ (document).keydown(function(e) {
     jogo.pressionou[e.which] = true;
 });
 
-$(document).keyup(function(e) {
+
+$ (document).keyup(function (e) {
     jogo.pressionou[e.which] = false;
 });
 
@@ -39,6 +42,9 @@ jogo.timer = setInterval(loop, 30);
 function loop() {  // Inicio função loop
     moveFundo();
     moveJogador();
+    moveInimigo1();
+    moveInimigo2();
+    moveAmigo();
 } // Final função loop
 
 
@@ -53,14 +59,53 @@ function moveJogador() { // Inicio função moveJogador
     if (jogo.pressionou[TECLA.W]) {
         var topo = parseInt($("#jogador").css("top"));
         $("#jogador").css("top",topo-10);
+        
+        if(topo <= 1) {
+            $("#jogador").css("top",topo+10);
+        }
     }
 
     if (jogo.pressionou[TECLA.S]) {
         var topo = parseInt($("#jogador").css("top"));
         $("#jogador").css("top",topo+10);
+
+        if(topo >=434) {
+            $("#jogador").css("top", topo-10);
+        }
     }
 
     if (jogo.pressionou[TECLA.D]) {
         // função disparo
     }
 } // fim função moveJogador
+
+function moveInimigo1() { // Inicio função moveInimigo1
+    posicaoX = parseInt($("#inimigo1").css("left"));
+    $("#inimigo1").css("left", posicaoX-velocidade);
+    $("#inimigo1").css("top", posicaoY);
+        if(posicaoX <=0 ) {
+            posicaoY = parseInt(Math.random() * 334);
+            $("#inimigo1").css("left", 694);
+            $("#inimigo1").css("top", posicaoY);
+        }
+    
+
+} // Fim função moveInimigo1
+
+function moveInimigo2() {  // Inicio função moveInimigo2
+    posicaoX = parseInt($("#inimigo2").css("left"));
+    $("#inimigo2").css("left", posicaoX-3);
+
+        if(posicaoX <= 0) {
+            $("#inimigo2").css("left", 775);
+        }
+} // FIm função moveInimigo2
+
+function moveAmigo() { // inicio função moveAmigo
+    posicaoX = parseInt($("#amigo").css("left"));
+    $("#amigo").css("left", posicaoX+1);
+
+        if(posicaoX>906) {
+            $("#amigo").css("left", 0);
+        }
+} // Fim função moveAmigo
