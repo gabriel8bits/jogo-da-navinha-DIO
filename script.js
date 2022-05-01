6f28d68dfcf79d8d7bf7a16f2cce255b380efc6b
Gabriel <gabrielcpa123@gmail.com>
2022-05-01 10:02:11 -0300

    att js

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ function start() {  // inicio função start
 } // fim função start
 
 
-
 // Variaveis
 
 var jogo = {};
@@ -19,17 +18,19 @@ var TECLA = {
     D: 68
 }
 
+jogo.pressionou = [];
 // Verifica se o usuário pressionou alguma tecla	
 
-jogo.pressionou = [];
 
-$(document).keydown(function(e){
-jogo.pressionou[e.which] = true;
+
+$(document).keydown(function(e) {
+    jogo.pressionou[e.which] = true;
 });
 
 $(document).keyup(function(e) {
-jogo.pressionou[e.which] = false;
+    jogo.pressionou[e.which] = false;
 });
+
 
 // Game Loop
 
@@ -37,9 +38,29 @@ jogo.timer = setInterval(loop, 30);
 
 function loop() {  // Inicio função loop
     moveFundo();
+    moveJogador();
 } // Final função loop
+
+
 
 function moveFundo() { // Inicio função loop
     esquerda = parseInt($('#fundoGame').css("background-position"));
     $("#fundoGame").css("background-position", esquerda-1);
 } // Final função moveFundo
+
+function moveJogador() { // Inicio função moveJogador
+
+    if (jogo.pressionou[TECLA.W]) {
+        var topo = parseInt($("#jogador").css("top"));
+        $("#jogador").css("top",topo-10);
+    }
+
+    if (jogo.pressionou[TECLA.S]) {
+        var topo = parseInt($("#jogador").css("top"));
+        $("#jogador").css("top",topo+10);
+    }
+
+    if (jogo.pressionou[TECLA.D]) {
+        // função disparo
+    }
+} // fim função moveJogador
